ae8635e5a773726642543d86d6c3c630d217ddfe
Prajwal Raj <prajwalraj224@gmail.com>
2023-04-11 22:58:37 +0530

    Add files via upload

---
+++ table-printer/table_prinmter.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# table_printer.py - formats lists of string into a tabular format
+
+
+def build_table(data):
+    """
+    Formats and builds a table from a nested list of strings.
+    Each individual element of the list represents a column
+
+    :param list data: list of strings
+    :return: str formatted for tabular representation
+    """
+    column_widths = []
+    output = ''
+
+    # list comprehension adds the largest length within the nested list
+    # to the column_widths list (with + 1 for buffer)
+    for col in data:
+        column_widths.append(max([len(element) for element in col]) + 1)
+
+    # uses indices to shift the table (data[0] is column, not row)
+    for col in enumerate(data[0]):
+        for row in enumerate(data):
+            output += '{}'.format(data[row[0]][col[0]].rjust(column_widths[row[0]]))
+        output += '\n'
+
+    return output
+
+
+if __name__ == "__main__":
+    table_data = [['apples', 'oranges', 'cherries', 'banana'],
+                  ['Alice', 'Bob', 'Carol', 'David'],
+                  ['dogs', 'cats', 'moose', 'goose']]
+
+    print(build_table(data=table_data))
